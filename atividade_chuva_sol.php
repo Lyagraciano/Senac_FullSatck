ae29ec4f204a544bf4972bc18d2f1d84b5124aae
Lyagraciano <148282919+Lyagraciano@users.noreply.github.com>
2023-11-27 11:12:20 -0300

    Add files via upload

---
+++ atividade_chuva_sol.php
@@ -0,0 +1,19 @@
+<?php
+//Juvêncio decidiu que dependendo do clima do dia seguinte ele decidiria o que fazer. Se o dia estiver ensolarado então ele vai para a praia. Se o dia estiver chuvoso então ele vai para a casa da mãe dele.
+$clima = "Ensolarado";
+
+if ($clima == "Ensolarado"){
+    echo "vai para a praia";
+}
+else{
+    echo "vai para a casa da mãe";
+}/*if ($clima =="ensolarado"){
+    echo "vai para a praia";
+}
+   else if ($clima=="chuvoso"){
+        echo "vai para a casa da mãe"
+    }
+    else{
+        echo "Juvêncio fica em sua própria casa";
+    }*/ 
+?>
