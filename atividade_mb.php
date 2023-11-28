ff0a8a630709de84e4ae7af24a2c447ae98f30e0
Lyagraciano <148282919+Lyagraciano@users.noreply.github.com>
2023-11-28 07:59:41 -0300

    Add files via upload

---
+++ atividade_mb.php
@@ -0,0 +1,14 @@
+<?php
+/*$MB = 100; 
+$Mbps = 5;
+$médiaDownload = $MB /$Mbps;
+echo "Tempo Estimado" . $médiaDownload ." segundos";*/
+
+//Calcular o valor das 3 medidas (altura, largura e comprimento) de uma caixa d'água retangular, calcule seu volume em m³ e depois calcule a sua capacidade em litros.
+/*$altura = 1.5;
+$largura = 4;
+$comprimento = 6;
+$volume = $altura * $largura * $comprimento;
+$capacidade = $volume * 1000;
+echo "Capacidade: $capacidade";*/
+?>
