6729198dcc89a386f17f5f91b94ff06dc53636cf
Lyagraciano <148282919+Lyagraciano@users.noreply.github.com>
2023-12-18 10:54:07 -0300

    santa_casa.js

--- santa_casa/santa_casa.js
+++ santa_casa/santa_casa.js
@@ -36,3 +36,11 @@ function V_cadastrar() {
     V_cpf(c);
     V_dataNascimento(d);
 }
+function formatCPF(campo){
+    let cpf = campo.value.replace(/\D/g,''); //remove todos os caracteres não numéricos.
+    cpf = cpf.substring(0,11); //limita o cpf a 11 dígitos (sem formatação)
+    cpf = cpf.replace(/(d{3})(\d)/, '$1.$2'); // adiciona o primeiro ponto
+    cpf = cpf.replace(/(d{3})(\d)/, '$1.$2'); //adiciona o segundo ponto
+    cpf = cpf.replace(/(d{3})(\d{$1,$2} $/, '$1-$2'); //
+
+}
